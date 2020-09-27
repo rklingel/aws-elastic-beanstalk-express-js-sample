0e9744700f7ae363f03239136ff9ea823fc0b449
Bob Klingel <rklingel@Bobs-MacBook-Pro.local>
2020-09-27 14:53:32 -0500

    second change with review

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Devops Tutorial testing Change #1'));
+app.get('/', (req, res) => res.send('Devops Tutorial testing Change #2 With review'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
